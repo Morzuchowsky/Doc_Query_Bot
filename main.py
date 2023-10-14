429cf79b4fbdfd8296bd105255d04fd25536cfe7
morzuchowsky <morzuchowsky@gmail.com>
2023-10-14 22:57:13 +0200

    Refactor and enhance user interaction in QA system
    - Improved variable and function naming for clarity and understanding.
    - Introduced comprehensive docstrings for all functions to enhance code documentation.
    - Enhanced user experience by:
      * Implementing `get_valid_file_path` to repeatedly prompt the user for a valid file path and allow exit options ('quit', 'wyjdz').
      * Introducing `ask_questions_until_exit` to let users ask questions continuously until they decide to exit.
    - Modularized code by breaking down the main execution process into separate functions:
      * load_documents_from_path
      * split_documents_into_chunks
      * create_and_save_vector_store
      * load_vector_store_and_setup_qa_system
    - Improved path input example by adding correct escape characters for Windows paths.

--- main.py
+++ main.py
@@ -5,27 +5,88 @@ from langchain.vectorstores import FAISS
 from langchain.chains import RetrievalQA
 from langchain.llms import OpenAI
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
-user_file_path = input("Please enter the file path (ex. \Adam\Desktop\lalka.pdf) : ")
 
-loader = PyPDFLoader(file_path=user_file_path)
-documents = loader.load()
-text_splitter = CharacterTextSplitter(
-    chunk_size=1000, chunk_overlap=30, separator="\n"
-)
+def get_valid_file_path():
+    """
+    Asks the user to input a file path and checks its validity.
+    Repeats until a valid file path is provided or the user decides to exit.
+    """
+    while True:
+        try:
+            file_path = input(
+                "Please enter the file path (ex. C:\\Adam\\Desktop\\lalka.pdf) or type 'quit' or 'wyjdz' to exit: ")
 
-docs = text_splitter.split_documents(documents=documents)
+            # Check if the user wants to quit
+            if file_path.lower() in ['quit', 'wyjdz']:
+                print("Exiting...")
+                exit()
 
-embeddings = OpenAIEmbeddings()
+            if os.path.exists(file_path):
+                return file_path
+            else:
+                print("The file path does not exist. Please try again.")
+        except Exception as e:
+            print(f"Error: {e}. Please try again.")
 
-vectorstore = FAISS.from_documents(docs, embeddings)
-vectorstore.save_local("faiss_index_react")
 
-new_vectorstore = FAISS.load_local("faiss_index_react", embeddings)
-qa = RetrievalQA.from_chain_type(
-    llm=OpenAI(), chain_type="stuff", retriever=new_vectorstore.as_retriever()
-)
-final_answer = qa.run(input("Please input Your question: "))
-print(final_answer)
+def ask_questions_until_exit(question_answering_system):
+    """
+    Allows the user to ask questions repeatedly until they type 'quit' or 'wyjdz'.
+    """
+    while True:
+        question = input("Please input your question (or type 'quit' or 'wyjdz' to exit): ")
+        if question.lower() in ['quit', 'wyjdz']:
+            break
+        answer = question_answering_system.run(question)
+        print(answer)
+
+
+def load_documents_from_path():
+    """
+    Load documents using the given file path.
+    """
+    document_loader = PyPDFLoader(file_path=file_path)
+    return document_loader.load()
+
+
+def split_documents_into_chunks(documents):
+    """
+    Split documents into manageable chunks.
+    """
+    text_splitter = CharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=30, separator="\n"
+    )
+    return text_splitter.split_documents(documents=documents)
+
+
+def create_and_save_vector_store(documents, embeddings):
+    """
+    Create a vector store from documents using the provided embeddings engine and save it locally.
+    """
+    vector_store = FAISS.from_documents(documents, embeddings)
+    vector_store.save_local("faiss_index_react")
+    return vector_store
+
+
+def load_vector_store_and_setup_qa_system(embeddings):
+    """
+    Load the locally saved vector store and set up the QA system.
+    """
+    loaded_vector_store = FAISS.load_local("faiss_index_react", embeddings)
+    return RetrievalQA.from_chain_type(
+        llm=OpenAI(), chain_type="stuff", retriever=loaded_vector_store.as_retriever()
+    )
+
+
+# Main execution
+file_path = get_valid_file_path()
+loaded_documents = load_documents_from_path()
+split_docs = split_documents_into_chunks(loaded_documents)
+embeddings_engine = OpenAIEmbeddings()
+create_and_save_vector_store(split_docs, embeddings_engine)
+qa_system = load_vector_store_and_setup_qa_system(embeddings_engine)
+ask_questions_until_exit(qa_system)
